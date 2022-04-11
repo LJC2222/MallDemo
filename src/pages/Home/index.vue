fe88a6aed700b65b4e02f82521a605c7ccb578a0
LJC2222 <2814508709@qq.com>
2022-04-11 10:04:25 +0800

    add picture to mock

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,36 +1,41 @@
 <template>
- <div class="home">
-   <TypeNav/>
-   <ListContainer/>
-   <TodayRecommend/>
-   <Rank/>
-   <Like/>
-   <Floor/>
-   <Floor/>
-   <Brand/>
- </div>
+  <div class="home">
+    <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Rank />
+    <Like />
+    <Floor :floor="floor" v-for="floor in floors" :key="floor.id" />
+    <Brand />
+  </div>
 </template>
 
 <script>
-import ListContainer from '@/pages/Home/ListContainer'
-import TodayRecommend from '@/pages/Home/TodayRecommend'
-import Rank from '@/pages/Home/Rank'
-import Like from '@/pages/Home/Like'
-import Floor from '@/pages/Home/Floor'
-import Brand from '@/pages/Home/Brand'
-
+import ListContainer from "@/pages/Home/ListContainer";
+import TodayRecommend from "@/pages/Home/TodayRecommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+import { mapState } from "vuex";
 export default {
-  components:{
+  components: {
     ListContainer,
     TodayRecommend,
     Rank,
     Like,
     Floor,
-    Brand
-  } 
-}
+    Brand,
+  },
+  mounted() {
+    this.$store.dispatch("getFloors");
+  },
+  computed: {
+    ...mapState({
+      floors: (state) => state.home.floors,
+    }),
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
