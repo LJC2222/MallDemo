1cd0426483d4c6981310694a9aaef4e050a748c4
LJC2222 <2814508709@qq.com>
2022-04-17 10:40:15 +0800

    添加三级菜单json文件，删除###连接

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -4,14 +4,14 @@
     <div class="container">
       <h2 class="all">全部商品分类</h2>
       <nav class="nav">
-        <a href="###">服装城</a>
-        <a href="###">美妆馆</a>
-        <a href="###">尚品汇超市</a>
-        <a href="###">全球购</a>
-        <a href="###">闪购</a>
-        <a href="###">团购</a>
-        <a href="###">有趣</a>
-        <a href="###">秒杀</a>
+        <a href="">服装城</a>
+        <a href="">美妆馆</a>
+        <a href="">尚品汇超市</a>
+        <a href="">全球购</a>
+        <a href="">闪购</a>
+        <a href="">团购</a>
+        <a href="">有趣</a>
+        <a href="">秒杀</a>
       </nav>
       <div class="sort">
         <div class="all-sort-list2">
